6c14a64a9de5b0cac723b9560223d4a674d2ab7d
Elroch <liamroche@yahoo.com>
2025-07-14 13:39:20 -0400

    7_partial

--- EmotionDetection/EmotionDetection/emotion_detection.py
+++ EmotionDetection/EmotionDetection/emotion_detection.py
@@ -9,5 +9,28 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json=input_json, headers=header)
     # Convert to json format for processing
     formatted_response = json.loads(response.text)
-    # return emotion probabilities
-    return formatted_response['emotionPredictions'][0]['emotion']
+    # Extract emotion probability evaluations
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    # Deal with failure
+    if response.status_code == 200:
+        emotions = {"anger": None, "disgust": None, "fear": None, 
+        "joy": None, "sadness": None}
+        dominant_emotion = None
+    else:
+    # Find dominant emotion
+        biggest_prob = -1
+        dominant_emotion = None
+        for emotion in emotions:
+            prob = emotions[emotion]
+            if prob > biggest_prob:
+                dominant_emotion = emotion
+                biggest_prob = emotions[emotion]
+    # Generate output string
+    output = "For the given statement, the system response is "
+    for emotion in emotions:
+        prob = emotions[emotion]
+        output = output + "'"+emotion+"': "+ str(prob) +", "
+    output = output[:-2] + ". The dominant emotion is " + str(dominant_emotion) + "."
+
+    # return string output
+    return output
